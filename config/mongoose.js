cb2915dc6bf9fb04f2b30477b113666240ec5e55
jignesh sharma <jigneshsharma9868@gmail.com>
2023-09-24 09:57:20 +0530

    connecting the mongodb with the atlas with the help of url

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 //const env = require("./environment");
+const url = 'mongodb+srv://jigneshsharma9868:evqbn7lRDlrGLlal@cluster0.ho9dsqw.mongodb.net/?retryWrites=true&w=majority';
 
 mongoose.connect(`mongodb://0.0.0.0:27017/habittracker`);
 
